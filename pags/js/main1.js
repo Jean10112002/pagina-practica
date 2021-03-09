b127425e11b353efd817451acfc5540c6f191e3a
Jean10112002 <78508734+Jean10112002@users.noreply.github.com>
2021-03-09 12:12:34 -0500

    Add files via upload

--- pags/js/main1.js
+++ pags/js/main1.js
@@ -236,7 +236,7 @@ const filtrar=()=>{
 const pintar=(e)=>{
 
     console.log('entraste a la funcion papu')
-    codeHtml+=`<div class="producto__celulares">
+    codeHtml+=`<div class="producto__celulares" data-aos="fade-right">
         <div class="producto__informacion">
         <div class="producto__img">
             <img src="${e.img}" alt="">
@@ -284,7 +284,7 @@ cajaDeFiltrar.addEventListener('click',(e)=>{
         codeHtml=''
         texto.style.display='initial'
         productos.forEach(e =>{
-            
+            contenedor.style.marginTop='6em'
             pintar(e)
             document.getElementById('todos').setAttribute('disabled','')
         })
@@ -294,6 +294,7 @@ cajaDeFiltrar.addEventListener('click',(e)=>{
 
 function filtrarPorDatos(a){
     texto.value=''
+    contenedor.style.marginTop='2.5em'
     document.getElementById('todos').style.display='initial'
     texto.style.display='none'
     productos.forEach(e  =>{
